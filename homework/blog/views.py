685c1a16c28dc9fd970d5063a98375a2b50dd276
suzie4831 <suzie4831@naver.com>
2019-05-08 21:39:26 +0900

    first commit

---
+++ homework/blog/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Blog
+from django.utils import timezone
+# Create your views here.
+def home(request):
+    blogs=Blog.objects
+    return render(request,'home.html',{'blogs':blogs})
+
+def detail(request, blog_id):
+    blog_detail = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'detail.html',{'blog':blog_detail})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    blog=Blog()
+    blog.title=request.GET['title']
+    blog.body=request.GET['body']
+    blog.pub_date=timezone.datetime.now()
+    blog.save()
+    return redirect('/blog/'+str(blog.id))
+
+def delete(request, blog_id):
+    blogs=Blog.objects.get(pk=blog_id)
+    blogs.delete()
+    return redirect('home')
+
+def edit(request, blog_id):
+    blog_edit= Blog.objects.get(pk=blog_id)
+    return render(request, 'edit.html',{'blog': blog_edit})
+
+def update(request, blog_id):
+    blog= Blog.objects.get(pk=blog_id)
+    blog.title=request.POST['title']
+    blog.pub_date=timezone.datetime.now()
+    blog.body=request.POST['body']
+    blog.save()
+    return redirect('home')
---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\suzie\desktop\0508\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
